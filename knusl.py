0ecd62cdf93271a96dbe93037481afce0ea6ea69
CodingIT <jungmin04030@naver.com>
2021-09-06 22:34:13 +0900

    Update knusl.py

--- knusl.py
+++ knusl.py
@@ -16,27 +16,27 @@
 
 import json
 
-class KnuSL():
+class KnuSL(): #단어 사전 클래스 생성
 
-	def data_list(wordname):	
-		with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-			data = json.load(f)
-		result = ['None','None']	
-		for i in range(0, len(data)):
-			if data[i]['word'] == wordname:
+	def data_list(wordname): #단어 어근, 극성 분석 함수 생성	
+		with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f: #단어 극성치 사전 파일 읽기 모드로 열기
+			data = json.load(f) #json 파일 디코딩
+		result = ['None','None'] #단어가 사전에서 검색되지 않을 경우에 반환값, 초기값	
+		for i in range(0, len(data)): 
+			if data[i]['word'] == wordname: #i번째 단어와 사전을 비교해서 단어가 존재하는지 확인 존재하면 아래 코드 실행
+				result.pop() #어근값과 극성치값 삭제
 				result.pop()
-				result.pop()
-				result.append(data[i]['word_root'])
-				result.append(data[i]['polarity'])	
+				result.append(data[i]['word_root']) #데이터 리스트에 어근 추가
+				result.append(data[i]['polarity'])	 #데이터 리스트에 극성치값 추가
 		
-		r_word = result[0]
-		s_word = result[1]
+		r_word = result[0] #단어의 어근
+		s_word = result[1] #단어의 극성치값
 							
 		print('어근 : ' + r_word)
 		print('극성 : ' + s_word)		
 		
 		
-		return r_word, s_word
+		return r_word, s_word #어근, 극성치값 반환
 
 if __name__ == "__main__":
 	
@@ -48,14 +48,35 @@ if __name__ == "__main__":
 	print("-2:매우 부정, -1:부정, 0:중립 or Unkwon, 1:긍정, 2:매우 긍정")
 	print("\n")	
 
-	while(True):
-		wordname = input("word : ")
-		wordname = wordname.strip(" ")		
-		if wordname != "#":
+	from konlpy.tag import Kkma #형태소 분석 라이브러리 임포트
+	kkma = Kkma() #형태소 분석 라이브러리 객체화
+	sent = []
+	x = kkma.pos("음식이 맛도 없고 딱딱해서 짜증나고 끔찍했어요.") #형태소 분석 후 데이터 저장 데이터 형식은 리스트 안 딕셔너리
+
+	score = 0
+	for i in range(len(x)):
+		if x[i][1] == "VA":
+			sent.append(x[i][0]+"다") #VA(형용사) + 어미 '다' 로 사전 기본형 생성
+		if x[i][1] == "VV":
+			sent.append(x[i][0]+"다") #VV(동사) + 어미 '다' 로 사전 기본형 생성
+		if x[i][1] == "MAG" and x[i + 1][1] == "VA":
+			sent.append(x[i][0]+x[i + 1][0]+"다")
+		y = ksl.data_list(x[i][0] + "다")
+		score += y[1] 	
+		if i == len(x):
+			score /= len(x)
+
+	print(x) 
+	for i in sent: print(i)
+
+	for i in sent: 
+		wordname = i
+		wordname = wordname.strip(" ") #큰따옴표 제거		
+		if wordname != "#": #사전 검색 유지
 			print(ksl.data_list(wordname))
 			print("\n")	
 				
 
-		elif wordname == "#":
+		elif wordname == "#": #사전 검색 종료
 			print("\n이용해주셔서 감사합니다~ :)")
 			break
